f78c80b92909d4e9268b08f2efdeccde6fd92df7
Batrick Swaistan <79750446+batrick-swaistan@users.noreply.github.com>
2022-08-11 22:30:57 +0530

    Update Linked_list.py

--- Linked_list.py
+++ Linked_list.py
@@ -1,23 +1,14 @@
-# A simple Python program for traversal of a linked list
-
-# Node class
 class Node:
-
-# Function to initialise the node object
     def __init__(self, data):
         self.data = data # Assign data
         self.next = None # Initialize next as null
 
-
-# Linked List class contains a Node object
 class LinkedList:
 
     # Function to initialize head
     def __init__(self):
         self.head = None
 
-    # This function prints contents of linked list
-    # starting from head
     def adddd(self,new_data):
         new_node=Node(new_data)
         new_node.next=self.head
@@ -29,20 +20,15 @@ class LinkedList:
             print (temp.data,end=" ")
             temp = temp.next
             
-        
-
-
-# Code execution starts here
+       
 if __name__=='__main__':
 
     # Start with the empty list
     llist = LinkedList()
-    llist.adddd(999)
     llist.head = Node(1)
     second = Node(2)
     third = Node(3)
-
     llist.head.next = second # Link first node with second
     second.next = third # Link second node with the third node
-
+    llist.adddd(5)
     llist.printList()
