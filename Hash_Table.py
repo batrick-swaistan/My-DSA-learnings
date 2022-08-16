a002de638c319ea781aec99e9365aec1e7fe8619
Batrick Swaistan <79750446+batrick-swaistan@users.noreply.github.com>
2022-08-16 21:59:28 +0530

    Create Hash_Table.py

---
+++ Hash_Table.py
@@ -0,0 +1,18 @@
+class Hashtable:
+    def __init__(self):
+        self.max=100
+        self.arr=[None for i in range(self.max)]
+    def get_hash(self,key):
+        h=0
+        for char in key:
+            h+=ord(char)
+        return h%self.max
+    def __setitem__(self,key,value):
+        h=self.get_hash(key)
+        self.arr[h]=value
+    def __getitem__(self,key):
+        h=self.get_hash(key)
+        return self.arr[h]
+    def __delitem__(self,key):
+        h=self.get_hash(key)
+        self.arr[h]=None
