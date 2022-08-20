21738e57f84397fa0bb75eb16528ad005697df5c
Batrick Swaistan <79750446+batrick-swaistan@users.noreply.github.com>
2022-08-20 21:16:35 +0530

    Update Hash_Table.py

--- Hash_Table.py
+++ Hash_Table.py
@@ -20,3 +20,35 @@ class Hashtable:
         h=self.get_hash(key)
         self.arr[h]=None
 
+#Handling Collision:
+
+
+class Hashtable:
+    def __init__(self):
+        self.max=10
+        self.arr=[[] for i in range(self.max)]
+    def hash_fun(self,key):
+        h=0
+        for char in key:
+            h+=ord(char)
+        return h%self.max
+    def __setitem__(self,key,value):
+        h=self.hash_fun(key)
+        found=False
+        for idx,ele in enumerate(self.arr[h]):
+            if len(ele)==2 and ele[0]==key:
+                self.arr[h][idx]=(key,value)
+                found=True
+                break
+        if not found:
+            self.arr[h].append((key,value))
+    def __getitem__(self,index):
+        h=self.hash_func(index)
+        return self.arr[h]
+    def __delitem__(self,key):
+        h=self.hash_func(key)
+        for index,ele in enumerate(self.arr[h]):
+            if ele[0]==key:
+                del self.arr[h][index]
+        
+        
