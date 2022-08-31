ff78319f9de967bdf78ae50bf258f2aa5f046ca8
Batrick Swaistan <79750446+batrick-swaistan@users.noreply.github.com>
2022-08-31 22:28:06 +0530

    Create graph.py

---
+++ graph.py
@@ -0,0 +1,42 @@
+class Graph:
+    def __init__(self,edges):
+        self.edges=edges
+        self.graph_dict={}
+        for start,end in edges:
+            if start in self.graph_dict:
+                self.graph_dict[start].append(end)
+            else:
+                self.graph_dict[start]=[end]
+        print(self.graph_dict)
+    
+    def get_path(self,start,end,path=[]):
+        path +=[start]
+        
+        if start == end:
+            return [path]
+        if start not in self.graph_dict:
+            return []
+        
+        paths=[]
+        for node in self.graph_dict[start]:
+            if node not in path:
+                new_paths=self.get_path(node,end,path)
+                for p in new_paths:
+                    paths.append(p)
+        return paths
+                
+if __name__=="__main__":
+    routes=[
+        ("Mumbai","Paris"),
+        ("Mumbai","Dubai"),
+        ("Paris","Dubai"),
+        ("Paris","New York"),
+        ("Dubai","New York"),
+        ("New York","Toronto")
+    ]
+    
+    start="Mumbai"
+    end="New York"
+    
+    nodes=Graph(routes)
+    print(nodes.get_path(start,end))
